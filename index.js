7646dbc31cb8e81a58a60d7873fc65e6fbf86a60
obadia-maasai <137788773+obadia-maasai@users.noreply.github.com>
2023-07-19 08:38:58 +0300

    Update index.js
    completed one

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31; 
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+const random =2
+    
+
+const num3 = 18;
+const num4 =  7;
+const mod = num3 % num4;
+
+const max =20;
